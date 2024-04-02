41da571cda2d715f4e18f7618659f0e8489704d3
Christine <tinegulmatico@gmail.com>
2024-04-02 09:24:35 +0800

    base files for springboot

---
+++ omas/src/main/java/com/omasystem/omas/OmasApplication.java
@@ -0,0 +1,13 @@
+package com.omasystem.omas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmasApplication.class, args);
+	}
+
+}
---
+++ omas/src/test/java/com/omasystem/omas/OmasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omasystem.omas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
