665c090c25b9f4ac52e32cc9c1a5f590da7dd945
Seiyrikon <rickygalpo7777@gmail.com>
2024-04-03 14:22:42 +0800

    ReservationRestController Updates Added: insertReservation Method(Not yet tested)

--- omas/src/main/java/com/omasystem/omas/Controller/ReservationRestController.java
+++ omas/src/main/java/com/omasystem/omas/Controller/ReservationRestController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.omasystem.omas.Model.ReservationInputBodyModel;
 import com.omasystem.omas.Service.ReservationService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,5 +27,11 @@ public class ReservationRestController {
     public Map<String, Object> getAllReservationPerSeat(@PathVariable Long seat_id)
     {
         return reservationService.getAllReservationPerSeat(seat_id);
+    }
+
+    @PostMapping("/{seat_id}/add")
+    public Map<String, Object> insertReservation(@PathVariable Long seat_id, @RequestBody ReservationInputBodyModel body)
+    {
+        return reservationService.insertReservation(seat_id, body);
     }
 }
