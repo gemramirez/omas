c7d954071886fd09b6cd521bb8659be388f55d6c
Seiyrikon <rickygalpo7777@gmail.com>
2024-04-03 14:21:56 +0800

    ReservationService Updates Added: insertReservation Method

--- omas/src/main/java/com/omasystem/omas/Service/ReservationService.java
+++ omas/src/main/java/com/omasystem/omas/Service/ReservationService.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.omasystem.omas.Dao.ReservationDao;
+import com.omasystem.omas.Dao.UserProjectDao;
+import com.omasystem.omas.Model.ReservationInputBodyModel;
 import com.omasystem.omas.Model.ReservationPerSeatModel;
+import com.omasystem.omas.Model.UserProjectModel;
 
 @Service
 public class ReservationService {
@@ -17,9 +20,16 @@ public class ReservationService {
     @Autowired
     private ReservationDao reservationDao;
 
+    @Autowired
+    private UserProjectDao userProjectDao;
+    
+    Map<String, Object> response = new HashMap<String, Object>();
+
+    // ** sample data for emp_id until the implementation of session using spring security.
+    public static final String EMP_ID = "101";
+
     //retrieves all reservation per seat and data are converted to string.
     public Map<String, Object> getAllReservationPerSeat(Long seat_id) {
-        Map<String, Object> response = new HashMap<String, Object>();
 
         try {
             List<ReservationPerSeatModel> reservationPerSeats = reservationDao.getAllReservationPerSeat(seat_id);
@@ -47,6 +57,30 @@ public class ReservationService {
                 response.put("message", stringList);
 
             }
+        } catch (Exception e) {
+            response.put("message", e.getMessage());
+        }
+        return response;
+    }
+
+    //saves reservation to database.
+    public Map<String, Object> insertReservation(Long seat_id, ReservationInputBodyModel body)
+    {
+        ReservationInputBodyModel bodyContainer = new ReservationInputBodyModel();
+
+        UserProjectModel currentProjectOfUser = userProjectDao.getProjectInvolvedOfUser(EMP_ID);
+
+        try {
+            bodyContainer.setEmp_id(EMP_ID);
+            bodyContainer.setSeat_id(Integer.parseInt(seat_id.toString()));
+            bodyContainer.setProj_id(currentProjectOfUser.getProj_id());
+            bodyContainer.setStart_date(body.getStart_date());
+            bodyContainer.setEnd_date(body.getEnd_date());
+            bodyContainer.setNote(body.getNote());
+
+            reservationDao.insertReservation(bodyContainer);
+
+            response.put("message", "Reservation Added Successfully");
         } catch (Exception e) {
             response.put("message", e.getMessage());
         }
