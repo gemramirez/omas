ca299297a758ae9f4481c107e7f6503bdd96a2b7
Christine <tinegulmatico@gmail.com>
2024-04-12 08:33:37 +0800

    added response messages to timetable

--- omas/src/main/java/com/omasystem/omas/Controller/TimetableController.java
+++ omas/src/main/java/com/omasystem/omas/Controller/TimetableController.java
@@ -1,5 +1,6 @@
 package com.omasystem.omas.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,30 +27,43 @@ public class TimetableController {
 
     /* Get the list of all reservations */
     @GetMapping("/reservations")
-    public List<TimetableModel> getAllReservations() {
+    public Map<String, Object> getAllReservations()
+    {
         return timetableService.getAllReservations();
     }
 
     /* Get the list of reserved seats based on a specific time range */
     @GetMapping("/reservation")
-    public List<TimetableModel> getAllReservationPerTimeslot(@RequestParam("start_date") String startDate, @RequestParam("end_date") String endDate) {
-        
-        Map<String, String> params = Map.of("start_date", startDate, "end_date", endDate);
+    public Map<String, Object> getAllReservationPerTimeslot(@RequestParam("start_date") String startDate, @RequestParam("end_date") String endDate) {
+        Map<String, String> params = new HashMap<>();
+        params.put("start_date", startDate);
+        params.put("end_date", endDate);
 
         return timetableService.getAllReservationPerTimeslot(params);
     }
 
+
     /* Get the list of reserved seats based on its start date and seat id */
     @GetMapping("/reservation/start")
-    public List<TimetableModel> getAllReservationStartDate(@RequestParam("startDate") String startDate, @RequestParam("seatId") int seatId) {
-
+    public Map<String, Object> getAllReservationStartDate(@RequestParam("startDate") String startDate, @RequestParam("seatId") int seatId) {
         return timetableService.getAllReservationStartDate(startDate, seatId);
     }
+    
 
     /* Get all seats based on a reservation */
     @GetMapping("/reservations/seat")
-    public List<SeatModel> getAllSeatsInReservation(@RequestParam Long reservationId) {
-        return timetableService.getAllSeatsInReservation(reservationId);
+    public Map<String, Object> getAllSeatsInReservation(@RequestParam Long reservationId) {
+        Map<String, Object> response = new HashMap<>();
+    
+        try {
+            Map<String, Object> seatsResponse = timetableService.getAllSeatsInReservation(reservationId);
+            response.put("message", seatsResponse.get("message"));
+        } catch (Exception e) {
+            response.put("message", e.getMessage());
+        }
+    
+        return response;
     }
+    
         
 }
--- omas/src/main/java/com/omasystem/omas/Service/TimetableService.java
+++ omas/src/main/java/com/omasystem/omas/Service/TimetableService.java
@@ -1,5 +1,7 @@
 package com.omasystem.omas.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,24 +18,137 @@ public class TimetableService {
 
     @Autowired
     private TimetableDao timetableDao;
-    
+
     // Get the list of all reservations
-    public List<TimetableModel> getAllReservations() {
-        return timetableDao.getAllReservations();
+    Map<String, Object> reservationResponse = new HashMap<String, Object>();
+    public Map<String, Object> getAllReservations()
+
+    {
+        try {
+            List<TimetableModel> reservations = timetableDao.getAllReservations();
+
+            if (reservations.size() == 0) {
+                reservationResponse.put("message", "No reservations.");
+            } else {
+                List<Map<String, Object>> stringList = new ArrayList<>();
+
+                for (TimetableModel reservation : reservations) {
+                    Map<String, Object> reservationMap = new HashMap<>();
+                    reservationMap.put("reservation_id", reservation.getReservation_id());
+                    reservationMap.put("seat_id", reservation.getSeat_id());
+                    reservationMap.put("reservation_start_date", reservation.getStart_date());
+                    reservationMap.put("reservation_end_date", reservation.getEnd_date());
+                    reservationMap.put("project", reservation.getProject_name());
+                    reservationMap.put("client", reservation.getClient_name());
+                    reservationMap.put("note", reservation.getNote());
+
+                    stringList.add(reservationMap);
+                }
+
+                reservationResponse.put("message", stringList);
+            }
+        } catch (Exception e) {
+            reservationResponse.put("message", e.getMessage());
+        }
+        return reservationResponse;
+    }
+
+    // Get the list of reserved seats based on a specific time range
+    public Map<String, Object> getAllReservationPerTimeslot(Map<String, String> parameters) {
+        Map<String, Object> timeRangeResponse = new HashMap<>();
+        try {
+            List<TimetableModel> slotReservations = timetableDao.getAllReservationPerTimeslot(parameters);
+            if (slotReservations.isEmpty()) {
+                timeRangeResponse.put("message", "No reservations found.");
+            } else {
+                List<Map<String, Object>> stringList = new ArrayList<>();
+                for (TimetableModel slotReservation : slotReservations) {
+                    Map<String, Object> slotReservationMap = new HashMap<>();
+                    slotReservationMap.put("reservation_id", slotReservation.getReservation_id());
+                    slotReservationMap.put("seat_id", slotReservation.getSeat_id());
+                    slotReservationMap.put("reservation_start_date", slotReservation.getStart_date());
+                    slotReservationMap.put("reservation_end_date", slotReservation.getEnd_date());
+                    slotReservationMap.put("project", slotReservation.getProject_name());
+                    slotReservationMap.put("client", slotReservation.getClient_name());
+                    slotReservationMap.put("note", slotReservation.getNote());
+                    stringList.add(slotReservationMap);
+                }
+                timeRangeResponse.put("reservations", stringList);
+            }
+        } catch (Exception e) {
+            timeRangeResponse.put("message", e.getMessage());
+        }
+        return timeRangeResponse;
     }
     
-    // Get the list of reserved seats based on a specific time range
-    public List<TimetableModel> getAllReservationPerTimeslot(Map<String, String> parameters) {
-        return timetableDao.getAllReservationPerTimeslot(parameters);
-    }
 
     // Get the list of reserved seat based on its start date & seat id
-    public List<TimetableModel> getAllReservationStartDate(String startDate, int seatId) {
-        return timetableDao.getAllReservationStartDate(startDate, seatId);
-    }
+    Map<String, Object> startResponse = new HashMap<String, Object>();
 
-    // Get all seats based on a reservation 
-    public List<SeatModel> getAllSeatsInReservation(Long parameter) {
-        return timetableDao.getAllSeatsInReservation(parameter);
+    public Map<String, Object> getAllReservationStartDate(String startDate, int seatId) {
+        try {
+            List<TimetableModel> startReservations = timetableDao.getAllReservationStartDate(startDate, seatId);
+    
+            if (startReservations.size() == 0) {
+                startResponse.put("message", "No reservations.");
+            } else {
+                List<Map<String, Object>> stringList = new ArrayList<>();
+    
+                for (TimetableModel startReservation : startReservations) {
+                    Map<String, Object> startReservationMap = new HashMap<>();
+                    startReservationMap.put("reservation_id", startReservation.getReservation_id());
+                    startReservationMap.put("seat_id", startReservation.getSeat_id());
+                    startReservationMap.put("reservation_start_date", startReservation.getStart_date());
+                    startReservationMap.put("reservation_end_date", startReservation.getEnd_date());
+                    startReservationMap.put("project", startReservation.getProject_name());
+                    startReservationMap.put("client", startReservation.getClient_name());
+                    startReservationMap.put("note", startReservation.getNote());
+    
+                    stringList.add(startReservationMap);
+                }
+    
+                startResponse.put("message", stringList);
+            }
+        } catch (Exception e) {
+            startResponse.put("message", e.getMessage());
+        }
+        return startResponse;
+    }
+    
+
+    // Get all seats based on a reservation
+    public Map<String, Object> getAllSeatsInReservation(Long parameter) {
+        Map<String, Object> seatResponse = new HashMap<>();
+        {
+            try {
+                List<SeatModel> seatReservations = timetableDao.getAllSeatsInReservation(parameter);
+        
+                if(seatReservations.size() == 0)
+                {
+                    seatResponse.put("message", "No reservations.");
+                }
+                else
+                {
+                    List<Map<String, Object>> stringList = new ArrayList<>();
+        
+                    for (SeatModel seatReservation: seatReservations) {
+                        Map<String, Object> startReservationMap = new HashMap<>();
+                        startReservationMap.put("seat_id", seatReservation.getSeat_id());
+                        startReservationMap.put("seat_status", seatReservation.getSeat_status());
+                        startReservationMap.put("department_id", seatReservation.getDept_id());
+                        startReservationMap.put("project_id", seatReservation.getProj_id());
+                        startReservationMap.put("registration_date", seatReservation.getReg_date());
+                        startReservationMap.put("registration_id", seatReservation.getReg_id());
+                        startReservationMap.put("total_seats_reserved", seatReservation.getTotalSeatsReserved());
+                        stringList.add(startReservationMap);
+                    }
+        
+                    seatResponse.put("message", stringList);
+                }
+            } catch (Exception e) {
+                seatResponse.put("message", e.getMessage());
+            }
+            return seatResponse;
+        }        
     }
 }
